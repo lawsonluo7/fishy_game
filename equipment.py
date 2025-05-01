e1fa595ebf87b128f7c224180c11d1f7c319c51e
Lawson Luo <87785612+lawsonluo7@users.noreply.github.com>
2025-05-01 21:31:09 +0000

    composition over inheritance for `NewCatch` class

--- equipment.py
+++ equipment.py
@@ -1,10 +1,13 @@
-class NewCatch(dict):
+class NewCatch:
+    def __init__(self):
+        self.contents = {}  # composition over inheritance TODO: this will be a dict of items and quantities, e.g. {"cod": 3, "herring": 1}
+    
     def __str__(self):
-        return ""  # TODO: this will be a string representation of the catch, with the name and quantity
+        return ""  # TODO: this will be a string representation of the catches, with the name and quantity
 
 class NoFishingGear:
     def __init__(self):
-        self.chances = {"nothing": 100}  # placeholder, other classes will overwrite this
+        self.chances = {"nothing": 100}  # placeholder, other classes will overwrite this when inheriting
 
     def cast(self) -> NewCatch:
         return NewCatch()  # TODO: this will be a NewCatch object, will be generated based on the chances dict
